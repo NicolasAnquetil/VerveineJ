fd738e60239e6cdaeec28b8b6d032a6c98d9bfdc
anquetil <anquetil.nicolas@gmail.com>
2011-04-28 21:39:26 +0000

    Added test for Initializer block

--- test_src/ad_hoc/Card.java
+++ test_src/ad_hoc/Card.java
@@ -1,11 +1,9 @@
 package ad_hoc;
 
-import java.util.ArrayList;
-import java.util.List;
+public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES };
 
 public class Card {
-
-	public enum Rank { DEUCE, THREE, FOUR, FIVE, SIX,
+    public enum Rank { DEUCE, THREE, FOUR, FIVE, SIX,
         SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }
 
     private final Rank rank;
@@ -27,22 +25,15 @@ public class Card {
     }
 
     private static final List<Card> protoDeck = new ArrayList<Card>();
-/*
+
     // Initialize prototype deck
     static {
-        for (Suit s : Suit.values())
-            for (Rank r : Rank.values()) {
-            	if (r == Rank.QUEEN) {
-            		System.out.println("God save the Queen!");
-            		}
-            	protoDeck.add(new Card(r, s));
-            }
+        for (Suit suit : Suit.values())
+            for (Rank rank : Rank.values())
+                protoDeck.add(new Card(rank, suit));
     }
-*/
+
     public static ArrayList<Card> newDeck() {
         return new ArrayList<Card>(protoDeck); // Return copy of prototype deck
     }
-
 }
-
-public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES };
