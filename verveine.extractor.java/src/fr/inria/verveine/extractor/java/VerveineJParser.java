06139e48e6612cda9ea46ada2983de2daf27fffb
girba <girba@31355b70-93d7-483c-8aff-8a9c15b89d28>
2011-10-04 04:17:26 +0000

    added possibility to specify -autocp folder that will collect all jars recursively from that folder down and add them to the classpath of the parsing

--- verveine.extractor.java/src/fr/inria/verveine/extractor/java/VerveineJParser.java
+++ verveine.extractor.java/src/fr/inria/verveine/extractor/java/VerveineJParser.java
@@ -5,6 +5,7 @@ import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jdt.core.JavaCore;
@@ -20,9 +21,26 @@ import fr.inria.verveine.core.gen.famix.SourceLanguage;
 import fr.inria.verveine.core.gen.famix.Type;
 
 /**
- * A batch parser inspired from org.eclipse.jdt.internal.compiler.batch.Main (JDT-3.6)
- * run with:
- * java -cp lib/org.eclipse.jdt.core_3.6.0.v_A48.jar:../Fame:/usr/local/share/eclipse/plugins/org.eclipse.equinox.common_3.5.1.R35x_v20090807-1100.jar:/usr/local/share/eclipse/plugins/org.eclipse.equinox.preferences_3.2.301.R35x_v20091117.jar:/usr/local/share/eclipse/plugins/org.eclipse.core.jobs_3.4.100.v20090429-1800.jar:/usr/local/share/eclipse/plugins/org.eclipse.core.contenttype_3.4.1.R35x_v20090826-0451.jar:/usr/local/share/eclipse/plugins/org.eclipse.core.resources_3.5.2.R35x_v20091203-1235.jar:/usr/local/share/eclipse/plugins/org.eclipse.core.runtime_3.5.0.v20090525.jar:/usr/local/share/eclipse/plugins/org.eclipse.osgi_3.5.2.R35x_v20100126.jar:../Fame/lib/akuhn-util-r28011.jar:lib/fame.jar:bin fr.inria.verveine.extractor.java.VerveineJParser [files|directory]_to_parse
+ * A batch parser inspired from org.eclipse.jdt.internal.compiler.batch.Main
+ * (JDT-3.6) run with: java -cp
+ * lib/org.eclipse.jdt.core_3.6.0.v_A48.jar:../Fame:
+ * /usr/local/share/eclipse/plugins
+ * /org.eclipse.equinox.common_3.5.1.R35x_v20090807
+ * -1100.jar:/usr/local/share/eclipse
+ * /plugins/org.eclipse.equinox.preferences_3.2
+ * .301.R35x_v20091117.jar:/usr/local
+ * /share/eclipse/plugins/org.eclipse.core.jobs_3
+ * .4.100.v20090429-1800.jar:/usr/local
+ * /share/eclipse/plugins/org.eclipse.core.contenttype_3
+ * .4.1.R35x_v20090826-0451.
+ * jar:/usr/local/share/eclipse/plugins/org.eclipse.core
+ * .resources_3.5.2.R35x_v20091203
+ * -1235.jar:/usr/local/share/eclipse/plugins/org.
+ * eclipse.core.runtime_3.5.0.v20090525
+ * .jar:/usr/local/share/eclipse/plugins/org.
+ * eclipse.osgi_3.5.2.R35x_v20100126.jar
+ * :../Fame/lib/akuhn-util-r28011.jar:lib/fame.jar:bin
+ * fr.inria.verveine.extractor.java.VerveineJParser [files|directory]_to_parse
  */
 
 public class VerveineJParser extends VerveineParser {
@@ -30,19 +48,18 @@ public class VerveineJParser extends VerveineParser {
 	public static final String DEFAULT_CODE_VERSION = JavaCore.VERSION_1_5;
 
 	/**
-	 * Option: The version of Java expected by the parser 
+	 * Option: The version of Java expected by the parser
 	 */
 	protected String codeVers = null;
 
 	/**
-	 * Option: wether to generate local informations (local to a type) 
+	 * Option: wether to generate local informations (local to a type)
 	 */
 	protected boolean withLocal = true;
 
-
 	/**
-	 * The arguments that were passed to the parser
-	 * Needed to relativize the source file names
+	 * The arguments that were passed to the parser Needed to relativize the
+	 * source file names
 	 */
 	private Collection<String> argPath;
 	private Collection<String> argFiles;
@@ -62,43 +79,77 @@ public class VerveineJParser extends VerveineParser {
 		return new JavaSourceLanguage();
 	}
 
+	public static void printFnames(String sDir) {
+		File[] faFiles = new File(sDir).listFiles();
+		for (File file : faFiles) {
+			if (file.getName().matches("^(.*jar)")) {
+				System.out.println(file.getAbsolutePath());
+			}
+			if (file.isDirectory()) {
+				printFnames(file.getAbsolutePath());
+			}
+		}
+	}
+
+	public static List<String> collectAllJars(String sDir) {
+		File[] faFiles = new File(sDir).listFiles();
+		List<String> tmpPath = new ArrayList<String>();
+		for (File file : faFiles) {
+			if (file.getName().endsWith("jar")) {
+				tmpPath.add(file.getAbsolutePath());
+				System.out.println(file.getAbsolutePath());
+			}
+			if (file.isDirectory()) {
+				tmpPath.addAll(collectAllJars(file.getAbsolutePath()));
+			}
+		}
+		return tmpPath;
+	}
+
 	public void setOptions(String[] args) {
 
-		String[] classPath = new String[] { };
+		String[] classPath = new String[] {};
 		argPath = new ArrayList<String>();
 		argFiles = new ArrayList<String>();
-
 		int i = 0;
 		while (i < args.length && args[i].startsWith("-")) {
 			String arg = args[i++];
 
 			if (arg.equals("-h")) {
 				usage();
-			}
-			else if (arg.matches("-1\\.[1-7]") || arg.matches("-[1-7]")) {
+			} else if (arg.matches("-1\\.[1-7]") || arg.matches("-[1-7]")) {
 				setCodeVersion(arg);
-			}
-			else if (arg.equals("-l")) {
+			} else if (arg.equals("-l")) {
 				this.withLocal = false;
-			}
-			else if (arg.equals("-cp")) {
+			} else if (arg.equals("-autocp")) {
+				if (i < args.length) {
+					List<String> tmpPath = collectAllJars(args[i++]);
+					int oldlength = classPath.length;
+					int newlength = oldlength + tmpPath.size();
+					classPath = Arrays.copyOf(classPath, newlength);
+					for (int p = oldlength; p < newlength; p++) {
+						classPath[p] = tmpPath.get(p - oldlength);
+					}
+				} else {
+					System.err.println("-autocp requires a root folder");
+				}
+			} else if (arg.equals("-cp")) {
 				if (i < args.length) {
 					String[] tmpPath = args[i++].split(System.getProperty("path.separator"));
 					int oldlength = classPath.length;
 					int newlength = oldlength + tmpPath.length;
 					classPath = Arrays.copyOf(classPath, newlength);
-					for (int p=oldlength; p < newlength; p++) {
-						classPath[p] = tmpPath[p-oldlength];
+					for (int p = oldlength; p < newlength; p++) {
+						classPath[p] = tmpPath[p - oldlength];
 					}
-				}
-				else {
+				} else {
 					System.err.println("-cp requires a classPath");
-				}	
-			}
-			else {
+				}
+			} else {
 				int j = super.setOption(i, args);
-				if (j > 0) {     // j is the number of args consumed
-					i += (j-1);  // 1 more will be added at the beginning of the loop ("args[i++]")
+				if (j > 0) { // j is the number of args consumed
+					i += (j - 1); // 1 more will be added at the beginning of
+									// the loop ("args[i++]")
 				}
 			}
 		}
@@ -112,8 +163,6 @@ public class VerveineJParser extends VerveineParser {
 			}
 		}
 
-		//tracePath("classPath", classPath);
-		//tracePath("sourcepath", argPath.toArray(new String[0]));
 		jdtParser.setEnvironment(classPath, argPath.toArray(new String[0]), null, true);
 		jdtParser.setResolveBindings(true);
 		jdtParser.setKind(ASTParser.K_COMPILATION_UNIT);
@@ -132,14 +181,16 @@ public class VerveineJParser extends VerveineParser {
 	}
 
 	protected void usage() {
-		System.err.println("Usage: VerveineJ [-h] [-l] [-cp CLASSPATH] [-1.1 | -1 | -1.2 | -2 | ... | -1.7 | -7] <files-to-parse> | <dirs-to-parse>");
+		System.err
+				.println("Usage: VerveineJ [-h] [-l] [-cp CLASSPATH] [-1.1 | -1 | -1.2 | -2 | ... | -1.7 | -7] <files-to-parse> | <dirs-to-parse>");
 		System.exit(0);
 
 	}
 
 	protected void setCodeVersion(String arg) {
 		if (codeVers != null) {
-			System.err.println("Trying to set twice code versions: " + codeVers + " and " + arg);
+			System.err.println("Trying to set twice code versions: " + codeVers
+					+ " and " + arg);
 			usage();
 		} else if (arg.equals("-1.1") || arg.equals("-1")) {
 			codeVers = JavaCore.VERSION_1_1;
@@ -159,7 +210,8 @@ public class VerveineJParser extends VerveineParser {
 
 	}
 
-	private void collectJavaFiles(Collection<String> paths, Collection<String> files) {
+	private void collectJavaFiles(Collection<String> paths,
+			Collection<String> files) {
 		for (String p : paths) {
 			collectJavaFiles(new File(p), files);
 		}
@@ -181,7 +233,7 @@ public class VerveineJParser extends VerveineParser {
 		VerveineJParser parser = new VerveineJParser();
 		parser.setOptions(args);
 		parser.parse();
-		//parser.debug();
+		// parser.debug();
 		parser.emitMSE(parser.getOutputFileName());
 	}
 
@@ -192,18 +244,20 @@ public class VerveineJParser extends VerveineParser {
 			this.expandNamespacesNames();
 		}
 
-		FamixRequestor req = new FamixRequestor(getFamixRepo(), argPath, argFiles, withLocal);
+		FamixRequestor req = new FamixRequestor(getFamixRepo(), argPath,
+				argFiles, withLocal);
 
 		sourceFiles.addAll(argFiles);
 		collectJavaFiles(argPath, sourceFiles);
-		jdtParser.createASTs(sourceFiles.toArray(new String[0]), null, new String[0], req, null);
+		jdtParser.createASTs(sourceFiles.toArray(new String[0]), null,
+				new String[0], req, null);
 
 		this.compressNamespacesNames();
 	}
 
 	/**
-	 * As explained in JavaDictionary, Namespaces are created with their fully qualified name.
-	 * We need now to give them their simple name
+	 * As explained in JavaDictionary, Namespaces are created with their fully
+	 * qualified name. We need now to give them their simple name
 	 */
 	private void compressNamespacesNames() {
 		for (Namespace ns : listAll(Namespace.class)) {
@@ -238,6 +292,5 @@ public class VerveineJParser extends VerveineParser {
 			}
 		}
 	}
-
 
 }
