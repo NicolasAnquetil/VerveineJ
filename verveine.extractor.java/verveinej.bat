5d346d7045a31633462ad36e90c2c21eb51bca89
girba <girba@31355b70-93d7-483c-8aff-8a9c15b89d28>
2011-10-04 04:11:41 +0000

    better bat

--- verveine.extractor.java/verveinej.bat
+++ verveine.extractor.java/verveinej.bat
@@ -1,27 +1,29 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
-# Directory for verveine source
-set ROOT=%CD%
-set BASELIB=%ROOT%\lib
+rem Directory for verveine source
+set ROOT=%~dp0%
+set BASELIB=%ROOT%lib
 
-# JVM options e.g. -Xmx2500m to augment maximum memory size of the vm to 2.5Go.
+rem JVM options e.g. -Xmx2500m to augment maximum memory size of the vm to 2.5Go.
 set JOPT="-Xmx2000m"
-# Verveine option
-VOPT="."
+rem Verveine option
+rem set VOPT="."
 
-CLASSPATH=${CLASSPATH}:${BASELIB}\verveine.extractor.java.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\fame.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\akuhn-util-r28011.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\famix.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\verveine.core.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.jdt.core_3.6.0.v_A58.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.core.contenttype_3.4.100.v20100505-1235.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.core.jobs_3.5.0.v20100515.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.core.resources_3.6.0.v20100526-0737.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.core.runtime_3.6.0.v20100505.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.equinox.common_3.6.0.v20100503.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.equinox.preferences_3.3.0.v20100503.jar
-CLASSPATH=${CLASSPATH}:${BASELIB}\org.eclipse.osgi_3.6.0.v20100517.jar
+set LOCALCLASSPATH=%BASELIB%\verveine.extractor.java.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\fame.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\akuhn-util-r28011.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\famix.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\verveine.core.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.jdt.core_3.6.0.v_A58.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.core.contenttype_3.4.100.v20100505-1235.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.core.jobs_3.5.0.v20100515.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.core.resources_3.6.0.v20100526-0737.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.core.runtime_3.6.0.v20100505.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.equinox.common_3.6.0.v20100503.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.equinox.preferences_3.3.0.v20100503.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%BASELIB%\org.eclipse.osgi_3.6.0.v20100517.jar;
+rem FOR /R . %%G IN (*.jar) DO set LOCALCLASSPATH=!LOCALCLASSPATH!;%%G
+set CLASSPATH=%LOCALCLASSPATH%
 
-
-java %JOPT% -cp %CLASSPATH% fr.inria.verveine.extractor.java.VerveineJParser %VOPT%
+java %JOPT% fr.inria.verveine.extractor.java.VerveineJParser %1 %2 %3 %4 %5 %6 %7 %8 %9
