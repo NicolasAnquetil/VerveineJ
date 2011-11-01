68256c6c81f0ce9ae68e1a0c01b847f915a6bef7
Tudor Girba <249595+girba@users.noreply.github.com>
2011-11-01 06:34:07 +0000

    changed class path to compute based on what is in the lib folder

--- verveinej.sh
+++ verveinej.sh
@@ -35,19 +35,11 @@ else
 	JOPT=""
 fi
 
-CLASSPATH="${CLASSPATH}:${BASELIB}/verveine.extractor.java.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/fame.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/akuhn-util-r28011.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/famix.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/verveine.core.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.jdt.core_3.6.0.v_A58.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.core.contenttype_3.4.100.v20100505-1235.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.core.jobs_3.5.0.v20100515.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.core.resources_3.6.0.v20100526-0737.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.core.runtime_3.6.0.v20100505.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.equinox.common_3.6.0.v20100503.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.equinox.preferences_3.3.0.v20100503.jar"
-CLASSPATH="${CLASSPATH}:${BASELIB}/org.eclipse.osgi_3.6.0.v20100517.jar"
+for i in $BASELIB/*.jar; do
+    CLASSPATH=$CLASSPATH:$i
+done
+CLASSPATH=`echo $CLASSPATH | cut -c2-`
 
+echo $CLASSPATH
 
 java $JOPT -cp $CLASSPATH fr.inria.verveine.extractor.java.VerveineJParser $VOPT
