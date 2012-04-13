3d1be8c705202fb30126f1620f2677fba53de9ce
larcheveque <larcheveque@31355b70-93d7-483c-8aff-8a9c15b89d28>
2012-04-13 14:10:46 +0000

    corrected Issue 763: 	VerveineJ generates wrong paths in source anchors when there is a . in the root folder

--- verveine.extractor.java/src/fr/inria/verveine/extractor/java/FamixRequestor.java
+++ verveine.extractor.java/src/fr/inria/verveine/extractor/java/FamixRequestor.java
@@ -1,6 +1,9 @@
 package fr.inria.verveine.extractor.java;
 
+import java.io.File;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.FileASTRequestor;
@@ -18,14 +21,24 @@ public class FamixRequestor extends FileASTRequestor {
 	 * Summarizing at the level of classes does not produce Method, Attributes, or Accesses, Invocation
 	 */
 	private boolean classSummary = false;
-
-	private Collection<String> argsDir;
-	private Collection<String> argsFile;
+	
+	Map<String, String> dirMap;
+	Map<String, String> fileMap;
 	
 	public FamixRequestor(Repository r, Collection<String> argsDir, Collection<String> argsFile, boolean classSummary) {
+		super();
 		this.famixRepo = r;
-		this.argsDir = argsDir;
-		this.argsFile = argsFile;
+		
+		this.fileMap = new HashMap<String, String>();
+		// initialization of the Map with the complete absolute path
+		for(String tempArgFile : argsFile)
+			this.fileMap.put(new File(tempArgFile).getAbsolutePath(), tempArgFile);
+
+		this.dirMap = new HashMap<String, String>();
+		// initialization of the Map with the complete absolute path
+		for(String tempArgDir : argsDir)
+			this.dirMap.put(new File(tempArgDir).getAbsolutePath(), tempArgDir);
+		
 		this.classSummary = classSummary;
 
 		this.famixDictionnary = new JavaDictionary(famixRepo);
@@ -44,18 +57,35 @@ public class FamixRequestor extends FileASTRequestor {
 		}
 	}
 
-	private String relativePath(String fullPath) {
-		for (String f : argsFile) {
-			if (fullPath.endsWith(f)) {
-				return f;
-			}
+	/**
+	 * Search in the program args the corresponding path of the provided fullPath
+	 * @param fullPath the provided fullPath
+	 * @return the corresponding path of the provided fullPath
+	 */
+	private String relativePath(String path) {
+
+		String fullPath = new File(path).getAbsolutePath();
+
+		if (this.fileMap.containsKey(fullPath)) {
+			return this.fileMap.get(fullPath);
 		}
 
-		for (String d : argsDir) {
-			int i = fullPath.indexOf(d);
-			if (i >= 0) {  // if fullPath contains d
-				return fullPath.substring(i);
+		File file = new File(fullPath);
+
+		while(file != null)
+		{
+
+			String key = file.getAbsolutePath();
+
+			if(this.dirMap.containsKey(key)){
+				// if key is dot, we add a / behind
+				if(this.dirMap.get(key).equals(".")) 
+					return "./" + fullPath.substring(key.length() + 1);
+				else 
+					return this.dirMap.get(key) + fullPath.substring(key.length() + 1);
 			}
+			else 
+				file = file.getParentFile();
 		}
 		return fullPath;
 	}
