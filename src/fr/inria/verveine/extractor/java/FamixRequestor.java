cf1abe299a2e3af5a7d9c8bb395e367bb978f096
Nicolas anquetil <nicolas.anquetil@inria.fr>
2021-04-22 18:19:48 +0200

    make imports explicit

--- src/fr/inria/verveine/extractor/java/FamixRequestor.java
+++ src/fr/inria/verveine/extractor/java/FamixRequestor.java
@@ -1,23 +1,23 @@
 package fr.inria.verveine.extractor.java;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.io.File;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import fr.inria.verveine.extractor.java.visitors.refvisitors.*;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.FileASTRequestor;
-import org.moosetechnology.model.famixjava.famixjavaentities.Method;
 
 import ch.akuhn.fame.Repository;
 import fr.inria.verveine.extractor.java.visitors.defvisitors.VisitorClassMethodDef;
 import fr.inria.verveine.extractor.java.visitors.defvisitors.VisitorComments;
 import fr.inria.verveine.extractor.java.visitors.defvisitors.VisitorPackageDef;
 import fr.inria.verveine.extractor.java.visitors.defvisitors.VisitorVarsDef;
+import fr.inria.verveine.extractor.java.visitors.refvisitors.VisitorAccessRef;
+import fr.inria.verveine.extractor.java.visitors.refvisitors.VisitorAnnotationRef;
+import fr.inria.verveine.extractor.java.visitors.refvisitors.VisitorExceptionRef;
+import fr.inria.verveine.extractor.java.visitors.refvisitors.VisitorInheritanceRef;
+import fr.inria.verveine.extractor.java.visitors.refvisitors.VisitorInvocRef;
+import fr.inria.verveine.extractor.java.visitors.refvisitors.VisitorTypeRefRef;
 
 public class FamixRequestor extends FileASTRequestor {
 
