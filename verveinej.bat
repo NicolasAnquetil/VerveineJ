bfbe2c5ecd45a360f40d8bb4e03a3728cdb30f6d
Gabriel Darbord <gabriel.darbord@gmail.com>
2024-02-14 12:51:00 +0100

    Use delayed expansion and escape paths

--- verveinej.bat
+++ verveinej.bat
@@ -1,17 +1,18 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
-rem Directory for verveine source
-set ROOT=%~dp0%
-set BASELIB=%ROOT%lib
+rem Directory for Verveine source
+set "ROOT=%~dp0"
+set "BASELIB=%ROOT%lib"
 
 rem JVM options e.g. -Xmx2500m to augment maximum memory size of the vm to 2.5Go.
-set JOPT="-Xmx16000m"
-rem Verveine option
-rem set VOPT="."
+set "JOPT=-Xmx16000m"
 
-FOR /R %BASELIB% %%G IN (*.jar) DO set LOCALCLASSPATH=%%G;!LOCALCLASSPATH!
-set CLASSPATH=%CLASSPATH%;%LOCALCLASSPATH%
+rem Verveine option
+rem set "VOPT=."
+
+FOR /R "%BASELIB%" %%G IN (*.jar) DO set "LOCALCLASSPATH=%%G;!LOCALCLASSPATH!"
+set "CLASSPATH=%CLASSPATH%;%LOCALCLASSPATH%"
 
 
 java -Dfile.encoding=UTF-8 %JOPT% fr.inria.verveine.extractor.java.VerveineJMain %*
